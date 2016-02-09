bf710e0b4590ea5de4ba34398e4fcfb5a049d63f
V2 Ray <admin@v2ray.com>
2016-02-09 21:01:52 +0000

    update

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+npm install gitbook-cli -g
+gitbook init
+gitbook build
+
+TARGET_DIR=_v2ray_com
+
+git clone "https://github.com/v2ray/v2ray.github.io.git" ${TARGET_DIR}
+
+rm -rf ${TARGET_DIR}/*
+cp -r _book/* ${TARGET_DIR}/
+cp favicon.png ${TARGET_DIR}/
+
+cd ${TARGET_DIR}
+
+git config user.name "V2Ray Auto Build"
+git config user.email "admin@v2ray.com"
+
+git add -A
+git commit -m 'update'
+git push "https://${GIT_KEY}@github.com/v2ray/v2ray.github.io.git" master
