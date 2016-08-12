72e26ba033c035fa3932882c6bce25bae8439fe2
Darien Raymond <admin@v2ray.com>
2016-08-12 16:37:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,14 @@ gitbook init
 gitbook build
 popd
 
+pushd blog
+npm install --save gitbook-plugin-anchors
+rsync -rv ../_layouts/ ./_layouts/
+rsync -rv ../resources/ ./resources/
+gitbook init
+gitbook build
+popd
+
 TARGET_DIR=_v2ray_com
 
 git clone "https://github.com/v2ray/v2ray.github.io.git" ${TARGET_DIR}
@@ -28,6 +36,8 @@ mkdir ${TARGET_DIR}/zh_cn/
 cp -r ./zh_cn/_book/* ${TARGET_DIR}/zh_cn/
 mkdir ${TARGET_DIR}/en/
 cp -r ./en/_book/* ${TARGET_DIR}/en/
+mkdir ${TARGET_DIR}/blog/
+cp -r ./blog/_book/* ${TARGET_DIR}/blog/
 cp CNAME ${TARGET_DIR}/
 cp robots.txt ${TARGET_DIR}/
 
