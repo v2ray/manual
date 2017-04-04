f071b398dc9802ff782a4f0dd2fbc18082f40732
Darien Raymond <admin@v2ray.com>
2017-04-04 23:08:10 +0200

    update

--- release.sh
+++ release.sh
@@ -4,7 +4,8 @@ PROJECT=$1
 
 echo "Launching build machine."
 DIR="$(dirname "$0")"
-gcloud compute instances create "v2raycom" \
+RAND="$(openssl rand -hex 5)"
+gcloud compute instances create "v2raycom-${RAND}" \
     --machine-type=n1-highcpu-2 \
     --metadata-from-file=startup-script=${DIR}/deploy.sh \
     --zone=us-west1-a \
