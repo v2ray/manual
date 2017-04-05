8b7d403f6c4f0360b427cd73d0705ac43fe7ab0d
Darien Raymond <admin@v2ray.com>
2017-04-05 23:43:20 +0200

    update

--- release.sh
+++ release.sh
@@ -9,4 +9,5 @@ gcloud compute instances create "v2raycom-${RAND}" \
     --machine-type=n1-highcpu-2 \
     --metadata-from-file=startup-script=${DIR}/deploy.sh \
     --zone=us-west1-a \
+    --preemptible \
     --project ${PROJECT}
