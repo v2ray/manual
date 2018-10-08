0f3173e1906d623d39fca3310f25d406260554fb
Darien Raymond <admin@v2ray.com>
2018-10-08 17:50:36 +0200

    update

--- cleanup.sh
+++ cleanup.sh
@@ -6,5 +6,5 @@ echo "Removing previous machines."
 gcloud compute instances list --project ${PROJECT} | grep TERMINATED \
 | awk '{printf "%s --zone %s\n", $1, $2}' \
 | while read LINE; do
-  yes Y | gcloud compute instances delete ${LINE} --project ${PROJECT}
+  gcloud --quiet compute instances delete ${LINE} --project ${PROJECT}
 done
